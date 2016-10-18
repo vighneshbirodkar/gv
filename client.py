a90dbd3ff63d610e298ef475be6a2a56198d31e6
Adithya Parthasarathy <adithya91@gmail.com>
2016-10-18 16:58:19 -0400

    Reducing evaluation space and increasing candidates

--- client.py
+++ client.py
@@ -7,26 +7,19 @@ import random
 import multiprocessing
 from joblib import Parallel, delayed
 
-BOARD_X = 1000
-BOARD_Y = 1000
-MIN_DIST_SQ = 66 ** 2
-candidate_limit = multiprocessing.cpu_count()
-
-pull_us = [[0] * 1000 for item in range(0, 1000)]
-pull_them = [[0] * 1000 for item in range(0, 1000)]
-
 def euclideanDistance(x1, y1, x2, y2):
     distance = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
     return distance
-    # return math.sqrt(distance)
 
 
 def get_move():
 
-    # Identify best move
-
-    spacing_x = BOARD_X / 10
-    spacing_y = BOARD_Y / 10
+    if (US == 2) and (moves_played == N):
+        spacing_x = BOARD_X / (SPACING_FACTOR * 5)
+        spacing_y = BOARD_Y / (SPACING_FACTOR * 5)
+    else:
+        spacing_x = BOARD_X / SPACING_FACTOR
+        spacing_y = BOARD_Y / SPACING_FACTOR
     
     current_x = spacing_x / 2
     current_y = spacing_y / 2
@@ -39,96 +32,69 @@ def get_move():
         while current_y <= BOARD_Y:
             if is_move_valid(current_x, current_y, stones):
                 candidates.append((current_x, current_y))
+                # print current_x, current_y
             current_y += spacing_y
         current_x += spacing_x
 
     # Evaluate candidates
-    # If player 1: Minimize distance between our moves (defensive)
-    # If player 2: Minimize distance between their moves (reactive)
-    #              If last turn, evaluate all and pick best
+    return get_candidate(candidates, stones)
 
 
-    # Deep evaluation
-    if US == 2 and moves_played == N:
-        return get_last_move(candidates, stones)
-    else:
-        return get_cadidate(candidates, stones)
-        
-
-def get_cadidate(candidates, stones):
+def get_candidate(candidates, stones):
 
     if (US == 1) and (moves_played == 1):
         return (BOARD_X / 2, BOARD_Y / 2)
 
-    
-    # Heuristic: Minimize with us, maximimize with us; minimize with them, maximize with them.
-    min_dist_us = 1e32
-    max_dist_us = -1
-    min_dist_them = 1e32
-    max_dist_them = -1
-
-    final_candidates = [ None ] * 4
-    
-    for candidate in candidates:
-        
-        # Compute distance to each move
-        current_dist_us = 0
-        current_dist_them = 0
-        
-        for move in stones:
-            
-            distance = euclideanDistance(candidate[0], candidate[1], move[0], move[1])
-            
-            if move[2] == US:
-                current_dist_us += distance
-
-            if move[2] == THEM:
-                current_dist_them += distance
-
-        # Update best candidates
-        if current_dist_us < min_dist_us:
-            final_candidates[0] = candidate
-            min_dist_us = current_dist_us
-
-        if current_dist_us > max_dist_us:
-            final_candidates[1] = candidate
-            max_dist_us = current_dist_us
-
-        if current_dist_them < min_dist_them:
-            final_candidates[2] = candidate
-            min_dist_them = current_dist_them
-
-        if current_dist_them > max_dist_them:
-            final_candidates[3] = candidate
-            max_dist_them = current_dist_them
-
-    # Add two random candidates
-    final_candidates.append(get_random_move())
-    final_candidates.append(get_random_move())
-
     # Identify best option among final candidates
     # Parallelize
     best_difference = -(BOARD_X * BOARD_Y)
-    idx = 1
 
-    scores = parallel_get_best_candidate_wrapper(final_candidates)
+    print('Evaluating', len(candidates), 'candidates')
 
-    for i in range(len(final_candidates)):
+    scores = parallel_get_best_candidate_wrapper(candidates)
+
+    for i in range(len(candidates)):
         (u, t) = scores[i]
+        difference = u - t
 
-        if (u - t) > best_difference:
-            best_difference = (u - t)
-            best_candidate = final_candidates[i]
-    
-    print(best_candidate, 'has scores', best_difference)
+        if difference > best_difference:
+            best_difference = difference
+            best_candidate = candidates[i]
+
+    print(best_candidate, 'Difference', best_difference)
+
+    # If this is the last move, make a deeper search around the best candidate to win
+    if (US == 2) and (moves_played == N) and (best_difference < 0):
+        target_candidates = len(candidates) / 2
+        candidates = []
+        box_size_x = BOARD_X / 10
+        box_size_y = BOARD_Y / 10
+        for i in range(len(target_candidates)):
+            randomI = random.randint(closest[0] - box_size_x, closest[0] + box_size_x)
+            randomJ = random.randint(closest[1] - box_size_y, closest[1] + box_size_y)
+
+            if is_move_valid(randomI, randomJ, stones):
+                candidates.append((randomI, randomJ))
+
+        # Re-evaluate among these candidates
+        scores = parallel_get_best_candidate_wrapper(candidates)
+        for i in range(len(candidates)):
+            (u, t) = scores[i]
+            difference = u - t
+            if difference > 0:
+                print('Winning local random found', candidates[i], 'Difference', best_difference)
+                return candidates[i]
+
+        # Return something
+        return get_random_move(candidates)
+
+    # Normal flow, return best candidate
     return best_candidate
 
 
 def parallel_get_best_candidate_wrapper(candidates):
 
     num_cores = multiprocessing.cpu_count()
-
-    print('Starting', num_cores, 'parallel threads')
     scores = Parallel(n_jobs = num_cores)(delayed(parallel_get_best_candidate)(c) for c in candidates)
 
     return scores
@@ -138,64 +104,15 @@ def parallel_get_best_candidate(candidate):
     return update_score(candidate[0], candidate[1], US, True)
 
 
-def get_last_move(candidates, stones):
-    bestScore = 0
-    
-    print('Final move, Have to evaluate', len(candidates))
-
-    if len(candidates) > candidate_limit:
-        print('Reducing to ', candidate_limit)
-        candidates = random.sample(candidates, candidate_limit)
-
-    closest = None
-    closest_loss = 0
-
-    # Evaluate
-    scores = parallel_get_best_candidate_wrapper(candidates)
-
-    for i in range(len(candidates)):
-        (u, t) = scores[i]
-
-        if u > t:
-            return candidates[i]
-            break
-        else:
-            if (closest_loss == 0) or (t - u < closest_loss):
-                closest_loss = t - u
-                closest = candidate
-
-    # If control reached here, no win condition was found
-    # Search further with the closest solution
-    candidates = []
-    for i in range(len(candidate_limit)):
-        randomI = random.randint(closest[0] - spacing_x, closest[0] + spacing_x)
-        randomJ = random.randint(closest[1] - spacing_y, closest[1] + spacing_y)
-
-        if is_move_valid(randomI, randomJ, stones):
-            candidates.append((randomI, randomJ))
-
-    # Evaluate
-    scores = parallel_get_best_candidate_wrapper(candidates)
-    
-    for i in range(len(candidates)):
-        (u, t) = scores[i]
-
-        if u > t:
-            return candidates[i]
-            break
-
-
-    # No hopes
-    return get_random_move()
-
-
 def update_score(i, j, player, temp=False):
 
     score_us = 0
     score_them = 0
 
-    for x in range(0, BOARD_X):
-        for y in range(0, BOARD_Y):
+    step = 10
+
+    for x in range(0, BOARD_X, 10):
+        for y in range(0, BOARD_Y, 10):
 
             if x == i and y == j:
                 continue
@@ -284,6 +201,14 @@ game_running = True
 first_data = True
 grid = [[0] * 1000 for item in range(0, 1000)]
 
+BOARD_X = 1000
+BOARD_Y = 1000
+MIN_DIST = 66
+MIN_DIST_SQ = MIN_DIST ** 2
+SPACING_FACTOR = 15
+
+pull_us = [[0] * 1000 for item in range(0, 1000)]
+pull_them = [[0] * 1000 for item in range(0, 1000)]
 
 while game_running:
     recv_data = sock.recv(1024)
@@ -325,6 +250,7 @@ while game_running:
     try:
         x, y = get_move()
     except:
+        print('Normal evaluation failed, falling back to random')
         x, y = get_random_move()
 
     moves_played += 1
@@ -332,4 +258,5 @@ while game_running:
     update_score(x, y, US)
 
     print('Making move (%d, %d)' % (x, y))
+    print('------------------------------------------------------')
     sock.sendall("%d %d" % (x, y))
---
+++ longnames.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+module load python-2.7.12
+
+python client.py $1
